40bef2a6a3d5c4866d7e9711efd5e38c72e65328
alantsd <alantsd@hotmail.com>
2020-01-18 14:48:02 +0800

    Update bitset.h
    add bitset_set_n()
    add bitset_reset_n()

--- inc/bitset.h
+++ inc/bitset.h
@@ -30,34 +30,66 @@
  *
  */
 
-#define BIT_SET_STORAGE_ELEMENT_BIT		(32)
+#define BIT_SET_STORAGE_ELEMENT_BIT			(32)
 #define BIT_SET_STORAGE_LEN(bit_count)		(bit_count / BIT_SET_STORAGE_ELEMENT_BIT)
 
 typedef uint32_t	bitset_storage_t;
 
 typedef struct bitset
 {
-	int				size;
+	/*uint8_t				ecc[8]*/
+	int						size;
 	bitset_storage_t*		bits;
 }
 bitset_t;
 
+#define START_BIT					0x80000000u
+
+#define	BIT_SET_1_BIT				0x80000000u
+#define	BIT_SET_2_BIT				0xc0000000u
+#define	BIT_SET_3_BIT				0xe0000000u
+#define	BIT_SET_4_BIT				0xf0000000u
+#define	BIT_SET_5_BIT				0xf8000000u
+#define	BIT_SET_6_BIT				0xfc000000u
+#define	BIT_SET_7_BIT				0xfe000000u
+#define	BIT_SET_8_BIT				0xff000000u
+
+#define	BIT_RESET_1_BIT				(BIT_SET_1_BIT)
+#define	BIT_RESET_2_BIT				(BIT_SET_2_BIT)
+#define	BIT_RESET_3_BIT				(BIT_SET_3_BIT)
+#define	BIT_RESET_4_BIT				(BIT_SET_4_BIT)
+#define	BIT_RESET_5_BIT				(BIT_SET_5_BIT)
+#define	BIT_RESET_6_BIT				(BIT_SET_6_BIT)
+#define	BIT_RESET_7_BIT				(BIT_SET_7_BIT)
+#define	BIT_RESET_8_BIT				(BIT_SET_8_BIT)
+
+
 void		bitset_init				(bitset_t* set);
-void		bitset_set				(bitset_t* set, int bit_pos);
+
+/** not support multi-bit set/reset across BIT_SET_STORAGE_ELEMENT_BIT boundary
+*	@param count - use BIT_SET_X_BIT or BIT_RESET_X_BIT as count argument.
+*/
+void		bitset_set_n			(bitset_t* set, int bit_pos, uint32_t count);
+void		bitset_reset_n			(bitset_t* set, int bit_pos, uint32_t count);
+
+static inline
+void		bitset_set				(bitset_t* set, int bit_pos) { bitset_set_n(set,  bit_pos, BIT_SET_1_BIT); }
+
+static inline
+void		bitset_reset			(bitset_t* set, int bit_pos) { bitset_reset_n(set,  bit_pos, BIT_RESET_1_BIT); }
+
 void		bitset_reset			(bitset_t* set, int bit_pos);
 void		bitset_toggle			(bitset_t* set, int bit_pos);
 
-bool        bitset_is_set			(bitset_t const* set, int bit_pos);
-bool        bitset_is_clr			(bitset_t const* set, int bit_pos);
+bool		bitset_is_set			(bitset_t const* set, int bit_pos);
+bool		bitset_is_clr			(bitset_t const* set, int bit_pos);
 
 /** find bit index */
-int		bitset_first_set_bit		(bitset_t* set);
-int		bitset_first_clr_bit		(bitset_t* set);
+int			bitset_first_set_bit	(bitset_t* set);
+int			bitset_first_clr_bit	(bitset_t* set);
 
-int		bitset_last_set_bit	(bitset_t* set);
-int		bitset_last_clr_bit	(bitset_t* set);
+int			bitset_last_set_bit		(bitset_t* set);
+int			bitset_last_clr_bit		(bitset_t* set);
 
 /** No logical operation - I don't see common needs for this yet */
-
-uint32_t	reverse_u32(uint32_t x);
 #endif
